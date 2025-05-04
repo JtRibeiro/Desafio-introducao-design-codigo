cbd15103f28bb1530def4326a692c55e16f9da20
JtRibeiro <torezonne@gmail.com>
2025-05-04 12:22:07 -0300

    feat: Adding calculators routes

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,7 +1,17 @@
 from flask import Blueprint, jsonify, request
+from src.main.factories.calculator4_factory import calculator4_factory
+
+from src.errors.error_controller import handle_errors
 
 calc_bp = Blueprint("calc_routes", __name__)
 
 @calc_bp.route("/calculator/4", methods=["POST"])
 def calculator4():
-    return jsonify({"message": "Olá mundo"})
+    try:
+        calc = calculator4_factory()
+        response = calc.calculate(request)
+        return jsonify(response), 200
+    except Exception as exception:
+        error_response = handle_errors(exception)
+        return jsonify(error_response["body"]), error_response["status_code"]
+    
